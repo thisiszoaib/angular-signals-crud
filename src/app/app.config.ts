2af162909fa1471a6010c17007a817fe1f4f86fb
Zoaib <thisiszoaib@gmail.com>
2023-06-14 13:06:49 +0500

    Firebase config removed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,14 +6,8 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCfHjWcPDlecLG5rFco6kLQi70aoQCjZok',
-  authDomain: 'angular-signals-crud.firebaseapp.com',
-  projectId: 'angular-signals-crud',
-  storageBucket: 'angular-signals-crud.appspot.com',
-  messagingSenderId: '922593477182',
-  appId: '1:922593477182:web:8f893b210d150c35c1c76f',
-};
+// Replace with your firebase config
+const firebaseConfig = {};
 
 export const appConfig: ApplicationConfig = {
   providers: [
